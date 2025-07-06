a2e29bef50a61e544c2b2e45ae990c91d5eb9405
Mghiii <abd.mghimimi@gmail.com>
2025-07-06 12:07:21 +0100

    update-parking

--- src/components/Park.jsx
+++ src/components/Park.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./park.css"
+import "./park.css";
 
 export default function Park() {
   return (
@@ -10,11 +10,34 @@ export default function Park() {
       <div className="parking">
         <div className="ent">
           <div className="entrer"></div>
+          <i class="bi bi-caret-right"></i>
+          <i class="bi bi-caret-left"></i>
           <div className="entrer" id="et1"></div>
         </div>
         <div className="dec">
           <div className="decor"></div>
-          <div className="places"></div>
+          <div className="places">
+            <div class="frst">
+              <div className="place1"></div>
+              <div className="place2"></div>
+              <div className="place3"></div>
+              <div className="place4"></div>
+              <div className="place5"></div>
+              <div className="place6"></div>
+              <div className="place7"></div>
+              <div className="place8"></div>
+            </div>
+            <div class="scnd">
+              <div className="place9"></div>
+              <div className="place10"></div>
+              <div className="place11"></div>
+              <div className="place12"></div>
+              <div className="place13"></div>
+              <div className="place14"></div>
+              <div className="place15"></div>
+              <div className="place16"></div>
+            </div>
+          </div>
           <div className="decor"></div>
         </div>
       </div>
